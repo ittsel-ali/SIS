f14131748acc8e68fdd637ef75f53426814d3a11
ittsel-ali <ittsel1@gmail.com>
2019-02-06 22:56:49 -0500

    Register SISContext for db during app iinitialization

--- SIS/Models/SISContext.cs
+++ SIS/Models/SISContext.cs
@@ -25,11 +25,10 @@ namespace SIS.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
+
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=localhost,1433;Database=SIS; User=SA; Password=ittselali91");
-            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
