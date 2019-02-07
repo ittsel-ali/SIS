4881097d56b7d81e432267b4f544743527fb104d
ittsel-ali <ittsel1@gmail.com>
2019-02-06 22:48:58 -0500

    minor changes and generate registration views and controller

---
+++ SIS/Controllers/RegistrationController.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SIS.Models;
+
+namespace SIS.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly SISContext _context;
+
+        public RegistrationController(SISContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Registration
+        public async Task<IActionResult> Index()
+        {
+            var sISContext = _context.Registration.Include(r => r.Course).Include(r => r.Student).Include(r => r.Term);
+            return View(await sISContext.ToListAsync());
+        }
+
+        // GET: Registration/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registration
+                .Include(r => r.Course)
+                .Include(r => r.Student)
+                .Include(r => r.Term)
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registration/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId");
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId");
+            ViewData["TermId"] = new SelectList(_context.StudyTerm, "TermId", "TermId");
+            return View();
+        }
+
+        // POST: Registration/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StudentId,CourseId,TermId")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(registration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", registration.CourseId);
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", registration.StudentId);
+            ViewData["TermId"] = new SelectList(_context.StudyTerm, "TermId", "TermId", registration.TermId);
+            return View(registration);
+        }
+
+        // GET: Registration/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registration.FindAsync(id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", registration.CourseId);
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", registration.StudentId);
+            ViewData["TermId"] = new SelectList(_context.StudyTerm, "TermId", "TermId", registration.TermId);
+            return View(registration);
+        }
+
+        // POST: Registration/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StudentId,CourseId,TermId")] Registration registration)
+        {
+            if (id != registration.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(registration);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegistrationExists(registration.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", registration.CourseId);
+            ViewData["StudentId"] = new SelectList(_context.Student, "StudentId", "StudentId", registration.StudentId);
+            ViewData["TermId"] = new SelectList(_context.StudyTerm, "TermId", "TermId", registration.TermId);
+            return View(registration);
+        }
+
+        // GET: Registration/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registration
+                .Include(r => r.Course)
+                .Include(r => r.Student)
+                .Include(r => r.Term)
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // POST: Registration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var registration = await _context.Registration.FindAsync(id);
+            _context.Registration.Remove(registration);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegistrationExists(int id)
+        {
+            return _context.Registration.Any(e => e.StudentId == id);
+        }
+    }
+}
